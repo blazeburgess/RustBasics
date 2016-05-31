bc6d75607ec3e5be2d70f1d7e1a6ab16f869cb15
thedailyephemera <thedailyephemera@gmail.com>
2016-05-31 19:47:13 -0400

    Added random number generation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,15 @@
 // 'prelude' section is always at the top
-use std::io; // this library allows user input
+extern crate rand; // extern for dependencies
+
+use std::io; // library that allows user input
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
